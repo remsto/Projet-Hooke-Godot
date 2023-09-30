ca12ccb5625bb4f4fa656f0c44e56bef06fc1dbc
remstoga <remi.gastaud@hotmail.com>
2023-09-30 14:00:23 +0200

    working right hook prototype
    The player can use the hook to grab terrain, only horizontally on the right

--- hook.gd
+++ hook.gd
@@ -1,16 +1,23 @@
-extends Node2D
+extends Area2D
 
-@export var max_stretch : float = 20.0
-@export var stretch_speed : float = 20.0
+@export var max_stretch : float = 10.0
+@export var stretch_speed : float = 30.0
+
+signal signal_pulling(new_speed : float)
 
 var pos_speed : float
+var hook_speed : float
 var is_active : bool
 var is_extending : float
+var is_pulling : float
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pos_speed = stretch_speed * $HookTip/CollisionShape2D.shape.size.x / 2.
+	pos_speed = stretch_speed * $CollisionShape2D.shape.size.x / 2.
+	hook_speed = stretch_speed * $CollisionShape2D.shape.size.x 
+#	hook_speed = 1000.0
 	is_active = false
 	is_extending = false
+	is_pulling = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,21 +25,28 @@ func _process(delta):
 	pass
 		
 func _physics_process(delta):
-	if Input.is_action_just_pressed("run"):
+	if Input.is_action_just_pressed("hook"):
 		is_active = true
 		is_extending = true
 	if is_active:
-		var init_x_pos = position.x
+		var init_x_pos = 0.0
+		# Extension phase
 		if is_extending:
-			scale.x += stretch_speed*delta
+#			scale.x += stretch_speed*delta
+			scale.x = move_toward(scale.x, max_stretch, stretch_speed*delta)
 			position.x += pos_speed*delta
 			if scale.x >= max_stretch:
 				is_extending = false
-				position.x = init_x_pos
+			if has_overlapping_bodies():
+				is_extending = false
+				is_pulling = true
+		# Retraction phase
 		else:
-			scale.x -= stretch_speed*delta
+			scale.x = move_toward(scale.x, 1.0, stretch_speed*delta)
 			position.x -= pos_speed*delta
-			if scale.x <= 1.0:
-				scale.x = 1.0
+			if is_pulling: 
+				signal_pulling.emit(pos_speed)
+			if scale.x == 1.0:
 				position.x = init_x_pos
 				is_active = false
+				is_pulling = false
--- player.gd
+++ player.gd
@@ -1,66 +1,64 @@
 extends CharacterBody2D
 
-const ACCEL = 20.0
+const ACCEL = 10000.0
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var is_being_pulled : bool
 var screen_size
 
 signal death
 
 func _ready():
-	position.x = 0.0
-	position.y = 0.0
+#	position.x = 0.0
+#	position.y = 0.0
 	$AnimatedSprite2D.animation = "idle"
 	$AnimatedSprite2D.play()
+	is_being_pulled = false
 	screen_size = get_viewport_rect().size
+	
 
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	var accel
 	var max_speed
-	if Input.is_action_pressed("run"): 
-		accel = ACCEL*2
-		max_speed = SPEED*2
+	if not $Hook.is_active:
+				# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		# Handle Jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if Input.is_action_pressed("run"): 
+			accel = ACCEL*2
+			max_speed = SPEED*2
+		else:
+			accel = ACCEL
+			max_speed = SPEED
+		if direction:
+			velocity.x = move_toward(velocity.x, direction*max_speed, accel)
+			$AnimatedSprite2D.animation = "walk"
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		else:
+			velocity.x = move_toward(velocity.x, 0, ACCEL)
+			$AnimatedSprite2D.animation = "idle"
 	else:
-		accel = ACCEL
-		max_speed = SPEED
-	if direction:
-#		velocity.x = direction * speed
-		velocity.x = move_toward(velocity.x, direction*max_speed, accel)
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimatedSprite2D.animation = "idle"
+		set_velocity(Vector2(0.0, 0.0))
+		if $Hook.is_pulling:
+			velocity.x = $Hook.hook_speed
 	move_and_slide()
 #	position = position.clamp(Vector2.ZERO, screen_size)
 
-#func die():
-#	'''
-#	Death of the player
-#	'''
-#	death.emit()
-#	queue_free()
-#
-#func _on_death_zone_body_entered(body):
-#	'''
-#	Death of the player
-#	'''
-#	print("oiuuiuisdcoiveqiu")
-#	die()
+
+
+
+func _on_hook_signal_pulling(new_pos):
+	is_being_pulled = true
